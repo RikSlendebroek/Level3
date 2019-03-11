7a8177abd4547d3aa8616a14e899f4c3408f5991
MagereHeinz <rik.slendebroek@gmail.com>
2019-03-11 21:34:43 +0100

    Viewholder and adapter

--- WebPortal/app/src/main/java/com/example/studentportal/Portal.java
+++ WebPortal/app/src/main/java/com/example/studentportal/Portal.java
@@ -3,4 +3,20 @@ package com.example.studentportal;
 public class Portal {
     private String url;
     private String label;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
--- WebPortal/app/src/main/java/com/example/studentportal/PortalViewHolder.java
+++ WebPortal/app/src/main/java/com/example/studentportal/PortalViewHolder.java
@@ -3,9 +3,14 @@ package com.example.studentportal;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 
 public class PortalViewHolder extends RecyclerView.ViewHolder {
+    public Button link;
+    public View view;
     public PortalViewHolder(@NonNull View itemView) {
         super( itemView );
+        view = itemView;
+        link = itemView.findViewById( R.id.button );
     }
 }
