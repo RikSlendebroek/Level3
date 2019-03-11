7a0abf0d3c27487a3ae7fdd1f97a8ca3c8d06010
MagereHeinz <rik.slendebroek@gmail.com>
2019-03-11 21:25:31 +0100

    Added base classes

---
+++ WebPortal/app/src/main/java/com/example/studentportal/PortalAdapter.java
@@ -0,0 +1,23 @@
+package com.example.studentportal;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class PortalAdapter extends RecyclerView.Adapter<PortalViewHolder> {
+    @NonNull
+    @Override
+    public PortalViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PortalViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ WebPortal/app/src/main/java/com/example/studentportal/PortalViewHolder.java
@@ -0,0 +1,11 @@
+package com.example.studentportal;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class PortalViewHolder extends RecyclerView.ViewHolder {
+    public PortalViewHolder(@NonNull View itemView) {
+        super( itemView );
+    }
+}
